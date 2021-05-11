b28c383a78a4db14a5ffd1bff7ec33093f6542a5
Apostrov <amir.subaev@gmail.com>
2021-05-11 11:42:52 +0300

    Update order struct. Add addOrder function

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 1. Install [Ganache](https://www.trufflesuite.com/ganache)
 1. Install [npm](https://nodejs.org/en/)
 1. Install Truffle `npm install --g truffle@5.1.39`
+1. Compile contract `truffle compile`
 1. Deploy contract `truffle migrate` or redeploy `truffle migrate --reset`
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,6 +9,12 @@ module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(NCoin);
   const ncoin = await NCoin.deployed();
 
+  // give all user 100 Acoin and 100 Ncoin
+  accounts.forEach(async (address) => {
+    await acoin.transfer(address, '100000000000000000000'); // 100 coins
+    await ncoin.transfer(address, '100000000000000000000');
+  });
+
   await deployer.deploy(OrderBook, acoin.address, ncoin.address);
   const orderBook = await OrderBook.deployed();
 };
