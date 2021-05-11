c24694ec7635a46f5731d82f2a2b055dd8616406
Salavat Dinmukhametov <perfog@mail.ru>
2021-05-11 13:20:19 +0300

    add account fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { configureRootTheme } from '@yandex/ui/Theme';
 import { theme } from '@yandex/ui/Theme/presets/default';
@@ -10,15 +10,46 @@ import { BuyPage } from './pages/BuyPage';
 import { SellPage } from './pages/SellPage';
 import { HomePage } from './pages/HomePage';
 
+import Web3 from 'web3';
+
 import './App.css';
 
 configureRootTheme({ theme });
 
 export const App: FC = () => {
+  const [account, setAccount] = useState<string>('');
+
+  useEffect(() => {
+    loadWeb3();
+    loadBlockchainData();
+  }, []);
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!'
+      );
+    }
+  };
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    const networkId = await web3.eth.net.getId();
+
+    setAccount(accounts[0]);
+  };
+
   return (
     <div className="App">
       <Router>
-        <Header />
+        <Header account={account} />
         <div className="Content">
           <Switch>
             <Route path="/innodex/buy">
